3307a91c0d0f4e8d075a1ceb0bfed846a2df02cc
Jeong Aine <75965933+aine-jeong@users.noreply.github.com>
2022-12-19 16:48:38 +0900

    FEAT: 2주차 1번문제 풀이

--- 2주차/ain/programmers2_1.java
+++ 2주차/ain/programmers2_1.java
@@ -10,9 +10,11 @@ public class programmers2_1 {
 
             for(int ing : ingredient) {
                 hamburger.push(ing);
+                // 스택의 크기가 4 이상일 때 부터 체크
                 if(hamburger.size() >= 4) {
                     int last = hamburger.size() -1;
                     if(hamburger.get(last) == 1 && hamburger.get(last-1) == 3 && hamburger.get(last-2) == 2 && hamburger.get(last-3) == 1) {
+                        //스택의 제일 위의 값 부터 빵(1), 고기(3), 야채(2), 빵(1) 이면 스택에서 제거하고 answer 1 추가
                         answer++;
                         hamburger.pop();
                         hamburger.pop();
